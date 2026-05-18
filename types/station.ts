45672d0387de9c538d3eec0d570d2cdbc4b50e14
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2026-05-18 19:10:56 +0000

    Add china.ts

--- types/station.ts
+++ types/station.ts
@@ -1,13 +1,16 @@
 export type Station = {
     city?: string;
+    stationID?: number;
     stationName: string
-    translations?: Array<translation>;
+    translations?: translation[];
     stationCode: string;
+    altCodes?: string[]
     sixLetterStationCode?: string;
     priority: number; // How high to prioritize the code, 0 is the code the operator uses, 1 is Secondary Operator, 2 is agregator, 3 is IATA
 }
 
 export type translation = {
     langCode: string;
-    text: string;
+    stationName?: string;
+    cityName?: string;
 }
