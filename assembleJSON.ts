cebb3887cb39cbaa64f6b388cce4a8020b14cb9e
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2026-05-18 19:11:03 +0000

    Create JSON

---
+++ assembleJSON.ts
@@ -0,0 +1,11 @@
+import { getAmtrakStations } from "./sources/amtrak";
+import { getChinaStations } from "./sources/china";
+import type { Station } from "./types/station";
+
+
+let data: Station[] = Array.prototype.concat(await Promise.all([
+    getAmtrakStations(),
+    getChinaStations(),
+]))
+
+Bun.write('./stations.json', JSON.stringify(data, null, 2))
