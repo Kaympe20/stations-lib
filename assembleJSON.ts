feaf6880301711b57e9d224611271a10a6dbc4d1
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2026-05-18 20:49:59 +0000

    Create csv

--- assembleJSON.ts
+++ assembleJSON.ts
@@ -1,7 +1,7 @@
 import { getAmtrakStations } from "./sources/amtrak";
 import { getChinaStations } from "./sources/china";
 import type { Station } from "./types/station";
-
+import papaparse = require('papaparse')
 
 let data: Station[] = Array.prototype.concat(...await Promise.all([
     getAmtrakStations(),
@@ -9,3 +9,17 @@ let data: Station[] = Array.prototype.concat(...await Promise.all([
 ]))
 
 Bun.write('./stations.json', JSON.stringify(data, null, 2))
+Bun.write('./stations.csv', papaparse.unparse({
+    fields: [
+        "city",
+        "stationID",
+        "stationName",
+        "aliases",
+        "translations",
+        "stationCode",
+        "longerStationCode",
+        "altCodes",
+        "priority",
+    ],
+    data: data,
+}))
