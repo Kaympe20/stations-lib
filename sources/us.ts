785536ebef56fc895c7c8f743a65f20b256190b6
Kayla Blosser <kayla.blosser@proton.me>
2026-05-18 01:01:43 -0400

    Improve types and pull amtrak

---
+++ sources/us.ts
@@ -0,0 +1,52 @@
+import type { Station } from "../types/station";
+
+export async function getUsStations() {
+    let data: apiResponse = await (await fetch(`https://services.arcgis.com/xOi1kZaI0eWDREZv/arcgis/rest/services/NTAD_Amtrak_Stations/FeatureServer/0/query?${new URLSearchParams({
+            where: "1=1",
+            outFields: "StaType,StnType,Name,Code,StationName,StationFacilityName,StationAliases,City",
+            outSR: "4326",
+            f: "json"
+        })}`)).json() as apiResponse;
+
+    // let stations: Array<Station> = [];
+
+    // for (let featuresMember of data.features) {
+    //     const stations: Station[] = data.features.map(({ attributes }) => ({
+    //         city: attributes.City,
+    //         stationName: attributes.Name,
+    //         stationCode: attributes.Code,
+    //         priority: 0,
+    //     }));
+    //     stations.push(newStation);
+    // }
+
+    const stations: Station[] = data.features.map(({ attributes }) => ({
+        
+
+        city: attributes.StationName,
+        stationName: !(attributes.Name === " ") ? attributes.Name : attributes.City,
+        stationCode: attributes.Code,
+        priority: 0,
+    }));
+
+    return stations;
+}
+
+//console.log(await getUsStations()) testing
+
+interface apiResponse {
+    features: Array<{
+        attributes: attributes;
+    }>
+}
+
+interface attributes {
+    StaType: string;
+    StnType: string;
+    Name: string;
+    Code: string;
+    StationName: string;
+    StationFacilityName: string;
+    StationAliases: string,
+    City: string;
+}
--- types/station.ts
+++ types/station.ts
@@ -1,8 +1,13 @@
-export type station = {
+export type Station = {
     city?: string;
     stationName: string
-    translations?: string;
+    translations?: Array<translation>;
     stationCode: string;
-    sixLetterStationCode: string;
+    sixLetterStationCode?: string;
+    priority: number; // How high to prioritize the code, 0 is the code the operator uses, 1 is Secondary Operator, 2 is agregator, 3 is IATA
 }
 
+export type translation = {
+    langCode: string;
+    text: string;
+}
